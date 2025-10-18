54fb03dedc01cc48ca4008e504543429d98f5390
LTC-117 <lucastentoni@gmail.com>
2025-10-18 19:40:20 -0300

    [Heap source file creation and basic foundations implementation]

---
+++ src/memory/heap/heap.c
@@ -0,0 +1,84 @@
+#include "heap.h"
+#include "../../kernel.h"
+#include "../../status.h"
+#include "../memory.h"
+
+#include <stdbool.h>
+#include <stdint.h>
+
+
+static int heap_validate_table(void *ptr, void *end, struct heap_table *table)
+{
+    int res = 0;
+
+    size_t table_size = (size_t)(end - ptr);
+    size_t total_blocks = table_size / MYOS_HEAP_BLOCK_SIZE;
+
+    if (table->total != total_blocks) {
+        res = -EINVARG;
+        goto out;
+    }
+
+out:
+    return res;
+}
+
+
+static bool heap_validate_alignment(void *ptr)
+{
+    return ((unsigned int)ptr % MYOS_HEAP_BLOCK_SIZE) == 0;
+}
+
+
+int heap_create(struct heap *heap, void *ptr, void *end, struct heap_table *table)
+{
+    int res = 0;
+
+    if (!heap_validate_alignment(ptr) || !heap_validate_alignment(end)) {
+        res = -EINVARG;
+        goto out;
+    }
+
+    memset(heap, 0, sizeof(struct heap));
+    heap->saddr = ptr;
+    heap->table = table;
+
+    res = heap_validate_table(ptr, end, table);
+    if (res < 0) {
+        goto out;
+    }
+
+    size_t table_size = sizeof(HEAP_BLOCK_TABLE_ENTRY) * table->total;
+    memset(table->entries, HEAP_BLOCK_TABLE_ENTRY_FREE, table_size); // set the whole table as free
+
+out:
+    return res;
+}
+
+
+static uint32_t heap_align_value_to_upper(uint32_t val)
+{
+    if ((val % MYOS_HEAP_BLOCK_SIZE) == 0) {
+        return val;
+    }
+
+    val = (val - (val % MYOS_HEAP_BLOCK_SIZE));
+    val += MYOS_HEAP_BLOCK_SIZE;
+
+    return val;
+}
+
+
+void *heap_malloc(size_t size)
+{
+    size_t aligned_size = heap_align_value_to_upper(size);
+
+    uint32_t total_blocks = aligned_size / MYOS_HEAP_BLOCK_SIZE;
+
+    return 0;
+}
+
+
+void heap_free(void *ptr)
+{
+}
