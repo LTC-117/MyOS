113c038913fb447ba80e9013d65a0176a08010b8
LTC-117 <lucastentoni@gmail.com>
2025-10-18 20:41:23 -0300

    [Testing the heap allocation and free functions]

--- src/kernel.c
+++ src/kernel.c
@@ -88,6 +88,10 @@ void kernel_main(void)
     void *ptr2 = kmalloc(5000);
     void *ptr3 = kmalloc(100);
 
-    if (ptr || ptr2 || ptr3) {
+    kfree(ptr);
+
+    void *ptr4 = kmalloc(50);
+
+    if (ptr || ptr2 || ptr3 || ptr4) {
     }
 }
