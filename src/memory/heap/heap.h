fa174cb648af7a0167bae6dcec6b82ac355b7a07
LTC-117 <lucastentoni@gmail.com>
2025-10-18 19:41:06 -0300

    [Heap header file creation and basic foundations implementation]

---
+++ src/memory/heap/heap.h
@@ -0,0 +1,38 @@
+#ifndef __HEAP_H__
+#define __HEAP_H__
+
+#include <stdint.h>
+#include <stddef.h>
+
+#include "../../config.h"
+
+
+#define HEAP_BLOCK_TABLE_ENTRY_TAKEN    0x01
+#define HEAP_BLOCK_TABLE_ENTRY_FREE     0x00
+
+// Table masks
+#define HEAP_BLOCK_HAS_NEXT             0b10000000
+#define HEAP_BLOCK_IS_FREE              0b01000000
+
+
+typedef unsigned char HEAP_BLOCK_TABLE_ENTRY;
+
+
+struct heap_table {
+    HEAP_BLOCK_TABLE_ENTRY *entries;
+    size_t total;
+};
+
+
+struct heap {
+    struct heap_table *table;
+    void *saddr;
+};
+
+
+int heap_create(struct heap *heap, void *ptr, void *end, struct heap_table *table);
+void *heap_malloc(size_t size);
+void heap_free(void *ptr);
+
+
+#endif
