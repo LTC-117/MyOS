908030f8372bb7bdf753fb937c461fc5ab89136d
LTC-117 <lucastentoni@gmail.com>
2025-10-22 16:14:48 -0300

    [Enabling paging]

--- src/kernel.c
+++ src/kernel.c
@@ -1,10 +1,12 @@
 #include "kernel.h"
 
-#include <stddef.h>
-#include <stdint.h>
 #include "idt/idt.h"
 #include "io/io.h"
 #include "memory/heap/kheap.h"
+#include "memory/paging/paging.h"
+
+#include <stddef.h>
+#include <stdint.h>
 
 
 uint16_t *video_mem = 0;
@@ -73,6 +75,7 @@ void print(const char *str)
     }
 }
 
+static struct paging_4gb_chunk *kernel_chunk = 0;
 
 void kernel_main(void)
 {
@@ -81,6 +84,15 @@ void kernel_main(void)
 
     // initialize interrupt descriptor table
     idt_init();
+
+    // Setup paging
+    kernel_chunk = paging_new_4gb(PAGING_IS_WRITEABLE | PAGING_IS_PRESENT | PAGING_ACCESS_FROM_ALL);
+
+    // Switch to kernel paging chunk
+    paging_switch(paging_4gb_chunk_get_directory(kernel_chunk));
+
+    // Enable paging
+    enable_paging();
 
     // Enable system interrupts
     enable_interrupts();
