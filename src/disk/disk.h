9fe9357fb026ba3c330cae7f6b37c67e97f98a4c
LTC-117 <lucastentoni@gmail.com>
2025-10-30 20:28:50 -0300

    [Improving the disk driver: adding the new implementations and enhancements in the driver's header file]

--- src/disk/disk.h
+++ src/disk/disk.h
@@ -2,7 +2,20 @@
 #define __DISK_H__
 
 
-int disk_read_sector(int lba, int total, void *buf);
+typedef unsigned int MYOS_DISK_TYPE;
+
+#define MYOS_DISK_TYPE_REAL     0  /* Represents a real physical hard disk */
+#define MYOS_DISK_SECTOR_SIZE   512
+
+struct disk {
+    MYOS_DISK_TYPE type;
+    int sector_size;
+};
+
+
+void disk_search_and_init(void);
+struct disk *disk_get(int index);
+int disk_read_block(struct disk *idisk, unsigned int lba, int total, void *buf);
 
 
 #endif
