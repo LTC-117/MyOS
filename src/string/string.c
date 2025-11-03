593c69d5eac7d953454fa697a441f9d361c67c6f
LTC-117 <lucastentoni@gmail.com>
2025-11-03 20:31:46 -0300

    [Implementing 'char to integer' and 'string to integer' convertion functions]

--- src/string/string.c
+++ src/string/string.c
@@ -1,5 +1,7 @@
 #include "string.h"
 
+#include "../kmath/kmath.h"
+
 
 int strlen(const char *str)
 {
@@ -11,4 +13,23 @@ int strlen(const char *str)
     }
 
     return i;
+}
+
+
+int ctoi(char c)
+{
+    return c - '0';
+}
+
+
+int strtoi(char *str)
+{
+    int len = strlen(str);
+    int num = 0;
+
+    for (int i = 0; i <= len; i++) {
+        num += (str[i] - '0') * (kpow(10, len));
+    }
+
+    return num;
 }
