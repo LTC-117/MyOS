1ccfc7f288369a50ed8ad1372e07458b74b6ba25
LTC-117 <lucastentoni@gmail.com>
2025-11-04 18:21:53 -0300

    [Renaming string module to avoid collisions with the C Library]

---
+++ src/kstring/kstring.c
@@ -0,0 +1,35 @@
+#include "kstring.h"
+
+#include "../kmath/kmath.h"
+
+
+int kstrlen(const char *str)
+{
+    int i = 0;
+
+    while (*str != 0) {
+        i++;
+        str += 1;
+    }
+
+    return i;
+}
+
+
+int kctoi(char c)
+{
+    return c - '0';
+}
+
+
+int kstrtoi(char *str)
+{
+    int len = kstrlen(str);
+    int num = 0;
+
+    for (int i = 0; i <= len; i++) {
+        num += (str[i] - '0') * (kpow(10, len));
+    }
+
+    return num;
+}
