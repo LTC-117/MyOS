58f828634c9a700cea3ea80d90b347db5e7a52f7
LTC-117 <lucastentoni@gmail.com>
2025-11-04 18:34:04 -0300

    [Making the 'io' functions visible]

--- src/io/io.h
+++ src/io/io.h
@@ -1,10 +1,20 @@
 #ifndef __IO_H__
 #define __IO_H__
 
-unsigned char insb(unsigned short port);
-unsigned short insw(unsigned short port);
+#include <stdint.h>
 
-void outb(unsigned short port, unsigned char val);
-void outw(unsigned short port, unsigned short val);
+extern unsigned char insb(unsigned short port);
+extern unsigned short insw(unsigned short port);
+
+extern void outb(unsigned short port, unsigned char val);
+extern void outw(unsigned short port, unsigned short val);
+
+
+void println(char *str);
+uint16_t terminal_make_char(char c, char color);
+void terminal_putchar(int x, int y, char c, char color);
+void terminal_write_char(char c, char color);
+void terminal_initialize(void);
+void print(const char *str);
 
 #endif
