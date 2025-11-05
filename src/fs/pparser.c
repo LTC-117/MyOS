7cdf455e870a2cf077130a53af2e17fdc7bc3ad8
LTC-117 <lucastentoni@gmail.com>
2025-11-05 20:19:58 -0300

    [Implementing function to validate file path]

--- src/fs/pparser.c
+++ src/fs/pparser.c
@@ -1,3 +1,18 @@
 #include "pparser.h"
 
+#include "../config.h"
+#include "../status.h"
+#include "../memory/memory.h"
+#include "../kstring/kstring.h"
 
+
+static int path_valid_format(const char *file_name)
+{
+    int len = kstrlen(file_name, MYOS_MAX_PATH);
+
+    if (len > MYOS_MAX_PATH) {
+        return -EFSSTR;
+    }
+
+    return (len >= 3) && (kisdigit(file_name[0])) && (memcmp((void *)&file_name[1], ":/", 2) == 0);
+}
