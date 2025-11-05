6c982fecb80a9ae9e5ac696f74debe0a5b4e4d59
LTC-117 <lucastentoni@gmail.com>
2025-11-05 20:26:34 -0300

    [Implementing function to get the drive from the file path passed]

--- src/fs/pparser.c
+++ src/fs/pparser.c
@@ -6,13 +6,28 @@
 #include "../kstring/kstring.h"
 
 
-static int path_valid_format(const char *file_name)
+static int pparser_path_valid_format(const char *file_name)
 {
     int len = kstrlen(file_name, MYOS_MAX_PATH);
 
     if (len > MYOS_MAX_PATH) {
-        return -EFSSTR;
+        return -EBADPATH;
     }
 
     return (len >= 3) && (kisdigit(file_name[0])) && (memcmp((void *)&file_name[1], ":/", 2) == 0);
+}
+
+
+static int pparser_get_drive_by_path(const char **path)
+{
+    if (!pparser_path_valid_format(*path)) {
+        return -EBADPATH;
+    }
+
+    int drive_no = kctoi(*path[0]);
+
+    // Add 3 bytes to skip drive number 0:/ 1:/ 2:/
+    *path += 3;
+
+    return drive_no;
 }
