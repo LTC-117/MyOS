c784b713036f407f242033b6acf624f5803663a1
LTC-117 <lucastentoni@gmail.com>
2025-11-06 22:04:52 -0300

    [Implementing 'disk streamer' module]

--- src/disk/streamer.h
+++ src/disk/streamer.h
@@ -11,4 +11,10 @@ struct disk_stream {
 };
 
 
+struct disk_stream *dstreamer_new(int disk_id);
+int dstreamer_seek(struct disk_stream *stream, int pos);
+int dstreamer_read(struct disk_stream *stream, void *out, int total);
+void dstreamer_close(struct disk_stream *stream);
+
+
 #endif
