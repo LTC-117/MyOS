0175fbcd7c6aa1c998906ac2f1b3f9c151d7b04f
LTC-117 <lucastentoni@gmail.com>
2025-11-06 22:03:17 -0300

    [Implementing 'disk streamer' module]

--- src/disk/streamer.c
+++ src/disk/streamer.c
@@ -1,3 +1,65 @@
 #include "streamer.h"
 
+#include "../memory/heap/kheap.h"
+#include "../config.h"
+#include "disk.h"
 
+
+struct disk_stream *dstreamer_new(int disk_id)
+{
+    struct disk *disk = disk_get(disk_id);
+
+    if (!disk) {
+        return 0;
+    }
+
+    struct disk_stream *streamer = kzalloc(sizeof(struct disk_stream));
+
+    streamer->pos = 0;
+    streamer->disk = disk;
+
+    return streamer;
+}
+
+
+int dstreamer_seek(struct disk_stream *stream, int pos)
+{
+    stream->pos = pos;
+    return 0;
+}
+
+
+int dstreamer_read(struct disk_stream *stream, void *out, int total)
+{
+    int sector = stream->pos / MYOS_DISK_SECTOR_SIZE;
+    int offset = stream->pos % MYOS_DISK_SECTOR_SIZE;
+    char buf[MYOS_DISK_SECTOR_SIZE];
+
+    int res = disk_read_block(stream->disk, sector, 1, buf);
+    if (res < 0) {
+        goto ret;
+    }
+
+    int total_to_read = (total > MYOS_DISK_SECTOR_SIZE)
+        ? MYOS_DISK_SECTOR_SIZE
+        : total;
+
+    for (int i = 0; i < total_to_read; i++) {
+        *(char *)out++ = buf[offset + i];
+    }
+
+    // Adjust the stream
+    stream->pos += total_to_read;
+    if (total > MYOS_DISK_SECTOR_SIZE) {
+        res = dstreamer_read(stream, out, total - MYOS_DISK_SECTOR_SIZE);
+    }
+
+ret:
+    return res;
+}
+
+
+void dstreamer_close(struct disk_stream *stream)
+{
+    kfree(stream);
+}
