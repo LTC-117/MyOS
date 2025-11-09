ad553f3f8f7c0adc109ded139d1d56e8d13d2ec0
LTC-117 <lucastentoni@gmail.com>
2025-11-08 21:24:16 -0300

    [Implementing 'container_of' and 'container_of_const' macros (latter is preferable)]

---
+++ src/myos/container_of.h
@@ -0,0 +1,23 @@
+#ifndef __MYOS_CONTAINER_OF_H__
+#define __MYOS_CONTAINER_OF_H__
+
+#include <stddef.h>
+#include <assert.h>
+
+
+#define typeof_member(T, m)     typeof(((T *)0)->m)
+
+#define container_of(ptr, type, member) ({                          \
+        void *_member_ptr = (void *)(ptr);                          \
+        static_assert(__same_type(*(ptr), ((type *)0)->member) ||   \
+                      __same_type(*(ptr), void),                    \
+                      "Pointer type mismatch in container_of()");   \
+        ((type *)(_member_ptr - offsetof(type, member))); })
+
+#define containter_of_const(ptr, type, member)                      \
+    _Generic(ptr,                                                   \
+            const typeof(*(ptr)) *: ((const type *)container_of(ptr, type, member)),\
+            default: ((type *)container_of(ptr, type, member))      \
+    )
+
+#endif
