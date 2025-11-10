2aa9e8648db29e3635afa46572441898299d83bf
LTC-117 <lucastentoni@gmail.com>
2025-11-09 22:22:52 -0300

    [Implementing the 'list add' real function]

--- src/myos/list.h
+++ src/myos/list.h
@@ -16,14 +16,21 @@ struct list_head {
     struct list_head name = LIST_HEAD_INIT(name)
 
 
-static __inline__ void __list_add(struct list_head *new_item,
-                                struct list_head *previous,
-                                struct list_head *next)
+static inline void __list_add(struct list_head *new_item,
+                                  struct list_head *previous,
+                                  struct list_head *next)
 {
     next->prev = new_item;
     new_item->prev = previous;
     new_item->next = next;
     previous->next = new_item;
+}
+
+
+static inline void list_add(struct list_head *new_item,
+                            struct list_head *head)
+{
+    __list_add(new_item, head, head->next);
 }
 
 
