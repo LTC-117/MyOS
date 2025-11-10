8ef26afa1245443ac1c2d84f1a0fb3c6a661d34f
LTC-117 <lucastentoni@gmail.com>
2025-11-09 22:21:09 -0300

    [Implementing the 'list add' template function]

--- src/myos/list.h
+++ src/myos/list.h
@@ -2,6 +2,9 @@
 #define __LIST_H__
 
 
+#include "container_of.h"
+
+
 struct list_head {
     struct list_head *prev, *next;
 };
@@ -11,6 +14,17 @@ struct list_head {
 
 #define LIST_HEAD(name) \
     struct list_head name = LIST_HEAD_INIT(name)
+
+
+static __inline__ void __list_add(struct list_head *new_item,
+                                struct list_head *previous,
+                                struct list_head *next)
+{
+    next->prev = new_item;
+    new_item->prev = previous;
+    new_item->next = next;
+    previous->next = new_item;
+}
 
 
 #endif
