06d34dc8f450b6cc631d95045a0e3c412880451f
LTC-117 <lucastentoni@gmail.com>
2025-12-29 15:23:23 -0300

    [Testing disk implementations]

--- src/kernel.c
+++ src/kernel.c
@@ -1,16 +1,20 @@
-#include "kernel.h"
-
 #include "idt/idt.h"
 #include "io/io.h"
 #include "memory/heap/kheap.h"
 #include "memory/paging/paging.h"
 #include "disk/disk.h"
+#include "disk/streamer.h"
 #include "kmath/kmath.h"
 #include "kstring/kstring.h"
 #include "fs/pparser.h"
+#include "fs/file.h"
+//#include "myos/container_of.h"
+#include "myos/list.h"
 
 #include <stddef.h>
 #include <stdint.h>
+
+#include "kernel.h"
 
 
 static struct paging_4gb_chunk *kernel_chunk;
@@ -36,4 +40,10 @@ void kernel_main(void)
 
     // Enable system interrupts
     enable_interrupts();
+
+    struct disk_stream *stream = dstreamer_new(0);
+    dstreamer_seek(stream, 0x201);
+    unsigned char c = 0;
+    dstreamer_read(stream, &c, 1);
+    while (1) {}
 }
